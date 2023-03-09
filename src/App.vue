a6a50dc0f3e742da82112d3ec8e67175429a9059
Samuel <samarandaf6@gmail.com>
2023-03-09 16:22:12 +0100

    cambios

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar
+    <!-- <v-app-bar
       app
       color="primary"
       dark
@@ -35,22 +35,25 @@
         <span class="mr-2">Latest Release</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
-    </v-app-bar>
+    </v-app-bar> -->
 
     <v-main>
       <HelloWorld/>
+      <HomePage/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+// import HelloWorld from './components/HelloWorld';
+import HomePage from './components/HomePage'
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    // HelloWorld,
+    HomePage,
   },
 
   data: () => ({
---
+++ src/components/HomePage.vue
@@ -0,0 +1,48 @@
+<template>
+    <v-card class="tarjeta" variant="tonal">
+        <div class="izquierda">
+            <img src="../photo/descarga.jpeg">
+        </div>
+        <div class="derecha">
+            <v-card-title>
+                carta
+            </v-card-title>
+            <v-card-subtitle>
+                Subtitulo
+            </v-card-subtitle>
+            <v-card-text>
+                sdfaslfuahflasduhfalkjhaslkgjahslkj
+            </v-card-text>
+            <v-card-actions>
+                <v-btn>Click me</v-btn>
+            </v-card-actions>
+        </div>
+    </v-card>
+</template>
+
+<script>
+export default {
+    name: 'HomePage',
+    data: () => ({
+    }),
+}
+</script>
+
+<style>
+.tarjeta {
+    display: flex;
+    width: 1000px;
+    height: 300px;
+}
+
+.derecha{
+    display: flex;
+    align-items: flex-start;
+    flex-direction: column;
+    width: calc(100% - 200px);
+}
+
+.izquierda{
+    width: 200px;
+}
+</style>
