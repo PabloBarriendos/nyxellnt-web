996fa3b93b5a17c492d83094433286b400d06dfe
Samuel <samarandaf6@gmail.com>
2023-06-08 00:00:20 +0200

    Google Analitics

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <HeaderComponent/>
+    <HeaderComponent />
     <router-view><v-content></v-content></router-view>
     <footerComponent />
   </v-app>
@@ -16,7 +16,7 @@ export default {
     HeaderComponent,
     footerComponent,
   },
-  data () {
+  data() {
     return {
     };
   },
@@ -24,6 +24,16 @@ export default {
   },
 
 };
+</script>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-0CETNH8XS0"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-0CETNH8XS0');
 </script>
 
 <style scoped>
