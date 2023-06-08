626667040b97d24b167db0678fe4c1085115cf97
Samuel <samarandaf6@gmail.com>
2023-06-08 17:28:09 +0200

    analytics

--- src/App.vue
+++ src/App.vue
@@ -26,16 +26,6 @@ export default {
 };
 </script>
 
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-0CETNH8XS0"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-0CETNH8XS0');
-</script>
-
 <style scoped>
 .v-main {
   padding: 0 !important;
