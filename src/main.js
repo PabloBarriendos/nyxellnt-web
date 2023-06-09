a70369fcdef10481b3b17c29153044f785a395c2
Samuel <samarandaf6@gmail.com>
2023-06-09 15:39:19 +0200

    añadida extension y codigo para gtm

--- src/main.js
+++ src/main.js
@@ -3,14 +3,21 @@ import App from "./App.vue";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import router from "./routes/index";
-// import VueAnalytics from "vue-analytics";
+import VueAnalytics from "vue-analytics";
+// import VueGtag from "vue-gtag";
 // import VueOptimize from "vue-optimize";
 
 Vue.config.productionTip = false;
 
-// Vue.use(VueAnalytics, {
-//   id: "G-0CETNH8XS0",
-//   router: router,
+Vue.use(VueAnalytics, {
+  id: "G-0CETNH8XS0",
+  router: router,
+});
+
+// Vue.use(VueGtag, {
+//   config: {
+//     id: "GTM-0CETNH8XS0",
+//   },
 // });
 
 // Vue.use(VueOptimize);
